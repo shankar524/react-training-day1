8237b824206bcebff70a3109b7ebe45e655d4f1d
User <user@SPMBP059.local>
2019-07-02 11:28:25 +0545

    added git ignore file

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'This is my first App.';
+
+ReactDOM.render(
+    <h1>{title}</h1>,
+    document.getElementById('app')
+);
